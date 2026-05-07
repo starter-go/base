bd4494af982f468c1560f1f61497c8a24474929f
kge <kge@bitwormhole.com>
2026-05-07 13:54:56 +0800

    实现了 CommonHolder 的所有方法

--- context2/holder.go
+++ context2/holder.go
@@ -18,17 +18,32 @@ type CommonHolder struct {
 
 // Context implements Holder.
 func (inst *CommonHolder) Context() context.Context {
-	panic("unimplemented")
+
+	f, err := inst.context.GetFacade()
+	if err != nil {
+		panic(err)
+	}
+	return f
 }
 
 // Getter implements Holder.
 func (inst *CommonHolder) Getter() Getter {
-	panic("unimplemented")
+
+	v, err := inst.context.GetValues()
+	if err != nil {
+		panic(err)
+	}
+	return v
 }
 
 // Setter implements Holder.
 func (inst *CommonHolder) Setter() Setter {
-	panic("unimplemented")
+
+	v, err := inst.context.GetValues()
+	if err != nil {
+		panic(err)
+	}
+	return v
 }
 
 func (inst *CommonHolder) _impl() Holder {
