34b1bfdcc69d45b050d2cafefa5aad4cab7d83a0
kge <kge@bitwormhole.com>
2026-05-07 13:46:02 +0800

    重新实现了 ConsoleAdapter.SetValue() , 以便保证调用该方法后 ,facade 的地址不会变化

--- context2/console.go
+++ context2/console.go
@@ -3,6 +3,7 @@ package context2
 import (
 	"context"
 	"fmt"
+	"time"
 )
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -24,10 +25,13 @@ func (inst *ConsoleAdapter) SetValue(c *Context, name any, value any) {
 
 	inst.innerCheckParams(c, name, value)
 
-	cc1 := c.Facade
-	cc2 := context.WithValue(cc1, name, value)
-	c.Facade = cc2
-	c.PutKey(name)
+	f1, err := c.GetFacade()
+	if err != nil {
+		panic(err)
+	}
+
+	f2 := f1.(*innerConsoleContextFacade)
+	f2.putValue(name, value)
 }
 
 func (inst *ConsoleAdapter) innerCheckParams(c *Context, name, value any) {
@@ -58,21 +62,91 @@ func (inst *ConsoleAdapter) _impl() Adapter {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+type innerConsoleContextFacade struct {
+	head context.Context
+	ctx2 *Context
+}
+
+// Deadline implements context.Context.
+func (inst *innerConsoleContextFacade) Deadline() (deadline time.Time, ok bool) {
+	h := inst.head
+	if h == nil {
+		t0 := time.Unix(0, 0)
+		return t0, false
+	}
+	return h.Deadline()
+}
+
+// Done implements context.Context.
+func (inst *innerConsoleContextFacade) Done() <-chan struct{} {
+	h := inst.head
+	if h == nil {
+		return nil
+	}
+	return h.Done()
+}
+
+// Err implements context.Context.
+func (inst *innerConsoleContextFacade) Err() error {
+	h := inst.head
+	if h == nil {
+		return nil
+	}
+	return h.Err()
+}
+
+// Value implements context.Context.
+func (inst *innerConsoleContextFacade) Value(key any) any {
+	h := inst.head
+	if h == nil {
+		return nil
+	}
+	return h.Value(key)
+}
+
+func (inst *innerConsoleContextFacade) putValue(name, value any) {
+
+	if name == nil || value == nil {
+		return
+	}
+
+	cc := inst.head
+	if cc == nil {
+		cc = context.Background()
+	}
+
+	cc = context.WithValue(cc, name, value)
+
+	inst.head = cc
+	inst.ctx2.PutKey(name)
+}
+
+func (inst *innerConsoleContextFacade) _impl() context.Context {
+	return inst
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 func SetupConsoleContext(cc context.Context) context.Context {
 
 	c1 := cc
-
 	if c1 == nil {
 		c1 = context.Background()
 	}
 
 	c2, err := Setup(c1, func(name string, ctx *Context) {
-		ctx.Adapter = new(ConsoleAdapter)
-		c3 := ctx.Facade
-		c3 = context.WithValue(c3, name, ctx)
-		ctx.Facade = c3
-	})
 
+		ada := new(ConsoleAdapter)
+		facade := new(innerConsoleContextFacade)
+
+		facade.ctx2 = ctx
+		facade.head = cc
+
+		ctx.Adapter = ada
+		ctx.Facade = facade
+
+		facade.putValue(name, ctx)
+	})
 	if err != nil {
 		panic(err)
 	}
